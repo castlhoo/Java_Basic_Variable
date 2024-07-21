625cbb80585355f85f533b1db9318928af1da689
castlehoo <ksungho9991@gmail.com>
2024-07-21 21:59:49 +0900

    Add files via upload

---
+++ VarEx1.java
@@ -0,0 +1,15 @@
+package variable.ex1;
+
+/*
+4,3을 다른 숫자로 한번에 변경할 수 있도록 num1, num2를 사용
+ */
+public class VarEx1 {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 3;
+
+        System.out.println("두 수의 합 = " +  (num1 + num2));
+        System.out.println("두 수의 곱 = " + (num1 * num2));
+        System.out.println("두 수의 뺼셈 = " + (num1 - num2));
+    }
+}
